55e761f9cb7e92a1f9523e1703bd12b7d3a4a3cf
RareformKRozhkov <45409688+RareformKRozhkov@users.noreply.github.com>
2020-05-06 15:38:56 -0400

    Adding Brute Force search method

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,22 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("CS 2223 - Final Project D-Term 2020\nGROUP MEMBERS: Cole Manning, Mason Powell, Liam McDonald, Sam Kwok, Sam Rowe\n------------");
     }
+
+    /**
+     * Uses brute force to search for text
+     * @param sentence Input sentence
+     * @param s String to search for
+     * @return Index of found text
+     */
+    public static int bruteStringSearch(String sentence, String s){
+        int index = -1;
+
+        for(int i = 0; i < (sentence.length() - s.length() + 1); i++){
+            if(sentence.substring(i,i+s.length()).equals(s)){
+                return i;
+            }
+        }
+
+        return index;
+    }
 }
