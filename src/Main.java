d4397a5dcf614f7cde83c185d84f85ae4a5018cb
RareformKRozhkov <45409688+RareformKRozhkov@users.noreply.github.com>
2020-05-06 14:57:01 -0400

    Initial Commit: main class created...

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("CS 2223 - Final Project D-Term 2020\nGROUP MEMBERS: Cole Manning, Mason Powell, Liam McDonald, Sam Kwok, Sam Rowe\n------------");
+    }
+}
