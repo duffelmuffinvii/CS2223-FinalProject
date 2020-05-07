e5db48da3ff8c1dff62c42f77215fcd318db4055
RareformKRozhkov <45409688+RareformKRozhkov@users.noreply.github.com>
2020-05-06 20:41:12 -0400

    JavaDoc for fileHandlerToString

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,13 @@ public class Main {
         System.out.println("CS 2223 - Final Project D-Term 2020\nGROUP MEMBERS: Cole Manning, Mason Powell, Liam McDonald, Sam Kwok, Sam Rowe\n------------");
     }
 
+    /**
+     * Converts txt (and maybe others depending on charset) to string.
+     * @param filename full file name (File should be in project/root directory).
+     * @param charset encoding charset.
+     * @return String representation of file.
+     * @throws FileNotFoundException if no file of that name is found.
+     */
     public static String fileHandlerToString(String filename, Charset charset) throws FileNotFoundException {
         String content = "";
         File FILE = new File("ElephantsChild.txt");
