68282bc81834173e4beb1182374dec9758c12982
RareformKRozhkov <45409688+RareformKRozhkov@users.noreply.github.com>
2020-05-06 20:36:30 -0400

    Tests for ElephantsChild.txt, and fcn for file IO.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         System.out.println("CS 2223 - Final Project D-Term 2020\nGROUP MEMBERS: Cole Manning, Mason Powell, Liam McDonald, Sam Kwok, Sam Rowe\n------------");
+        String STRING_ElephantsChild = fileHandlerToString("ElephantsChild.txt",StandardCharsets.ISO_8859_1);
+        System.out.println(STRING_ElephantsChild);
+    }
+
+    public static String fileHandlerToString(String filename, Charset charset) throws FileNotFoundException {
+        String content = "";
+        File FILE = new File("ElephantsChild.txt");
+        if (!FILE.exists()) throw new FileNotFoundException("File \""+FILE+"\" was not found");
+        Scanner scanner = new Scanner(FILE, String.valueOf(charset));
+        while (scanner.hasNextLine()) {
+            content = content.concat(scanner.nextLine().concat("\n"));
+        }
+        return content;
     }
 }
--- src/Tests.java
+++ src/Tests.java
@@ -1,13 +1,32 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Tests {
+
+    //File IO
+    Tests() throws FileNotFoundException {
+    }
+    String ElephantsChildTXT = Main.fileHandlerToString("ElephantsChild.txt",StandardCharsets.ISO_8859_1);
+
 
     @Test
     void baselineTest() {
         assertEquals(6,BruteForce.bruteStringSearch("Lorem ipsum dolor sit amet","ipsum"));
         assertEquals(6,Horspool.search("ipsum","Lorem ipsum dolor sit amet"));
         //todo liam's method
+    }
+
+    @Test
+    void elephantsChildTest() throws FileNotFoundException {
+        //file IO
+        String STRING_ElephantsChild = Main.fileHandlerToString("ElephantsChild.txt", StandardCharsets.ISO_8859_1);
+        //tests
+        assertEquals(33,BruteForce.bruteStringSearch(STRING_ElephantsChild,"Kipling"));
     }
 }
